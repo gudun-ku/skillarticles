709b6ed8667956d1024efde3c9bdd05772b67002
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-02-11 17:18:22 +0300

    Changes viewModel initialisation to using our ViewModelDelegate

--- app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/RootActivity.kt
@@ -42,11 +42,7 @@ class RootActivity : BaseActivity<ArticleViewModel>(), IArticleView {
 
     override val layout: Int = R.layout.activity_root
 
-    override val viewModel: ArticleViewModel by lazy {
-        val vmFactory = ViewModelFactory("0")
-        ViewModelProviders.of(this,vmFactory).get(ArticleViewModel::class.java)
-    }
-    //override val viewModel: ArticleViewModel by provideViewModel("0")
+    override val viewModel: ArticleViewModel by provideViewModel("0")
 
     @VisibleForTesting(otherwise = VisibleForTesting.PROTECTED)
     public override val binding: ArticleBinding by lazy { ArticleBinding() }
