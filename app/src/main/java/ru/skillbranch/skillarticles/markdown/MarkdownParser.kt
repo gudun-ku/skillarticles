ba12e91cbb60f034bc290d1902e4efab947694a6
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-02-20 00:01:52 +0300

    Adds parsing header, quote, italic, bold, strike, rule, inline code 1:24:00

--- app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/markdown/MarkdownParser.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.skillarticles.markdown
 
+import android.util.Log
 import java.util.regex.Pattern
 
 object MarkdownParser {
@@ -8,9 +9,19 @@ object MarkdownParser {
 
     // group regex
     private const val UNORDERED_LIST_ITEM_GROUP = "(^[*+-] .+)"
+    private const val HEADER_GROUP = "(^#{1,6} .+?$)"
+    private const val QUOTE_GROUP = "(^> .+?$)"
+    private const val ITALIC_GROUP = "((?<!\\*)\\*[^*].*?[^*]?\\*(?!\\*)|(?<!_)_[^_].*?[^_]?_(?!_))"
+    private const val BOLD_GROUP = "((?<!\\*)\\*{2}[^*].*?[^*]?\\*{2}(?!\\*)|(?<!_)_{2}[^_].*?[^_]?_{2}(?!_))"
+    private const val STRIKE_GROUP = "((?<!~)~{2}[^~].*?[^~]?~{2}(?!~))"
+    private const val RULE_GROUP = "(^[-_*]{3}$)"
+    private const val INLINE_GROUP = "((?<!`)`[^`\\s].*?[^`\\s]?`(?!`))"
+
 
     // result regex
-    const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP"
+    private const val MARKDOWN_GROUPS = "$UNORDERED_LIST_ITEM_GROUP|$HEADER_GROUP|$QUOTE_GROUP" +
+                                        "|$ITALIC_GROUP|$BOLD_GROUP|$STRIKE_GROUP|$RULE_GROUP" +
+                                        "|$INLINE_GROUP"
 
     private val elementsPattern by lazy {
         Pattern.compile(MARKDOWN_GROUPS, Pattern.MULTILINE)
@@ -56,7 +67,10 @@ object MarkdownParser {
             var text: CharSequence
 
             // groups range info
-            val groups = 1..1
+
+            val groups = 1..8
+
+            // every group
             var group = -1
             for (currGroup in groups) {
                 if(matcher.group(currGroup) != null) {
@@ -73,10 +87,81 @@ object MarkdownParser {
                     // text without "*. "
                     text = string.subSequence(startIndex.plus(2), endIndex)
                     // find inner
-                    val subs = findElements(text)
-                    val element = Element.UnorderedListItem(text, subs)
+                    val subelements = findElements(text)
+                    val element = Element.UnorderedListItem(text, subelements)
                     parents.add(element)
                     // next search starts from position "endIndex (last regex character)
+                    lastStartIndex = endIndex
+                }
+
+                // HEADER
+                2 -> {
+                    val reg = "^#{1,6}".toRegex().find(string.subSequence(startIndex, endIndex))
+                    val level = reg!!.value.length
+
+                    // text without "{#} "
+                    text = string.subSequence(startIndex.plus(level.inc()), endIndex)
+                    val element = Element.Header(level, text)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // QUOTE
+                3 -> {
+                    // text without "> "
+                    text = string.subSequence(startIndex.plus(2), endIndex)
+                    val subelements = findElements(text)
+                    val element = Element.Quote(text, subelements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // ITALIC
+                4 -> {
+                    // text without "*{}*"
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+                    val subelements = findElements(text)
+                    val element = Element.Italic(text, subelements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // BOLD
+                5 -> {
+                    // text without "**{}**"
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+                    val subelements = findElements(text)
+                    val element = Element.Bold(text, subelements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // STRIKE
+                6 -> {
+                    // text without "~~{}~~"
+                    text = string.subSequence(startIndex.plus(2), endIndex.minus(2))
+                    val subelements = findElements(text)
+                    val element = Element.Strike(text, subelements)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // RULE
+                7 -> {
+                    // text without "***" insert empty character
+                    text = " " + string.subSequence(startIndex.plus(3), endIndex)
+                    val element = Element.Rule(text)
+                    parents.add(element)
+                    lastStartIndex = endIndex
+                }
+
+                // INLINE
+                8 -> {
+                    // text without "`{}`"
+                    text = string.subSequence(startIndex.inc(), endIndex.dec())
+                    val subelements = findElements(text)
+                    val element = Element.InlineCode(text, subelements)
+                    parents.add(element)
                     lastStartIndex = endIndex
                 }
             }
@@ -103,6 +188,42 @@ sealed class Element() {
     ): Element()
 
     data class UnorderedListItem(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Header(
+        val level: Int = 1,
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Quote(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Italic(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Bold(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Strike(
+        override val text: CharSequence,
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class Rule(
+        override val text: CharSequence = " ", // for insert span
+        override val elements: List<Element> = emptyList()
+    ): Element()
+
+    data class InlineCode(
         override val text: CharSequence,
         override val elements: List<Element> = emptyList()
     ): Element()
--- app/src/test/java/ru/skillbranch/skillarticles/MarkdownParserTest.kt
+++ app/src/test/java/ru/skillbranch/skillarticles/MarkdownParserTest.kt
@@ -18,6 +18,104 @@ class MarkdownParserTest {
         printElements(result.elements)
     }
 
+    @Test
+    fun parse_header() {
+        val result = MarkdownParser.parse(headerString)
+        val actual = prepare<Element.Header>(result.elements)
+        val actualLevels = result.elements.spread()
+            .filterIsInstance<Element.Header>()
+            .map{ it.level }
+        assertEquals(expectedHeader, actual)
+        assertEquals(listOf(1,2,3,4,5,6), actualLevels)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_quote() {
+        val result = MarkdownParser.parse(quoteString)
+        val actual = prepare<Element.Quote>(result.elements)
+        assertEquals(expectedQuote, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_italic() {
+        val result = MarkdownParser.parse(italicString)
+        val actual = prepare<Element.Italic>(result.elements)
+        assertEquals(expectedItalic, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_bold() {
+        val result = MarkdownParser.parse(boldString)
+        val actual = prepare<Element.Bold>(result.elements)
+        assertEquals(expectedBold, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_strike() {
+        val result = MarkdownParser.parse(strikeString)
+        val actual = prepare<Element.Strike>(result.elements)
+        assertEquals(expectedStrike, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_combine() {
+        val result = MarkdownParser.parse(combineEmphasisString)
+        val actualItalic = prepare<Element.Italic>(result.elements)
+        val actualBold = prepare<Element.Bold>(result.elements)
+        val actualStrike = prepare<Element.Strike>(result.elements)
+        assertEquals(expectedCombine["italic"], actualItalic)
+        assertEquals(expectedCombine["bold"], actualBold)
+        assertEquals(expectedCombine["strike"], actualStrike)
+
+        printResults(actualBold)
+        printResults(actualItalic)
+        printResults(actualStrike)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_rule() {
+        val result = MarkdownParser.parse(ruleString)
+        val actual = prepare<Element.Rule>(result.elements)
+        assertEquals(3, actual.size)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
+    @Test
+    fun parse_inline_code() {
+        val result = MarkdownParser.parse(inlineString)
+        val actual = prepare<Element.InlineCode>(result.elements)
+        assertEquals(expectedInline, actual)
+
+        printResults(actual)
+        println("")
+        printElements(result.elements)
+    }
+
     private fun printResults(list: List<String>) {
         val iterator = list.iterator()
         while (iterator.hasNext()) {
