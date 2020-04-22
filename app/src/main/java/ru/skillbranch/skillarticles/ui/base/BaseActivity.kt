accde495c29f8b17661d87ea4067352379a93450
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-04-22 17:58:26 +0300

    First try to solve module 6

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -15,6 +15,7 @@ import androidx.core.view.children
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions.circleCropTransform
@@ -77,14 +78,31 @@ abstract class BaseActivity<T: BaseViewModel<out IViewModelState>>: AppCompatAct
             }
 
             is NavigationCommand.FinishLogin -> {
-                navController.navigate(R.id.finish_login)
-                if (command.privateDestination != null) navController.navigate(command.privateDestination)
+                navController.navigate(R.id.finish_login, null,
+                    NavOptions.Builder()
+                        .setLaunchSingleTop(true)
+                        .setPopUpTo(navController.currentDestination!!.id, true)
+                        .build())
+                if (command.privateDestination != null)
+                    navController.navigate(
+                        command.privateDestination)
+//                        null,
+//                        NavOptions.Builder()
+//                            .setLaunchSingleTop(true)
+//                            .setPopUpTo(command.privateDestination, true)
+//                            .build()
+//                    )
             }
+
 
             is NavigationCommand.StartLogin -> {
                 navController.navigate(
                     R.id.start_login,
-                    bundleOf("private_destination" to (command.privateDestination ?: -1))
+                    bundleOf("private_destination" to (command.privateDestination ?: -1)),
+                    NavOptions.Builder()
+                        .setLaunchSingleTop(true)
+                            .setPopUpTo(navController.currentDestination!!.id, true)
+                            .build()
                 )
             }
         }
